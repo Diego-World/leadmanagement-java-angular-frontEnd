1683117972d3e3e62153df0f2de89468fd9ed9cc
diegoDev <contato.diegogouveia@gmail.com>
2023-11-22 22:04:22 -0300

    Lead Display List - core

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LeadDisplayListComponent } from './lead-display-list/lead-display-list.component';
+import { PageBackgroundComponent } from './page-background/page-background.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LeadDisplayListComponent
+    LeadDisplayListComponent,
+    PageBackgroundComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/page-background/page-background.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page-background',
+  template: '<div class="background"></div>',
+  styleUrls: ['./page-background.component.css']
+})
+export class PageBackgroundComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+}
